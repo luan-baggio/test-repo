e119b41ec7120f5b4a30842fa3ac0d4a43b9123f
luabagg <luanbaggio0@gmail.com>
2020-01-30 13:52:17 -0300

    Insertion Sort

---
+++ sorting.php
@@ -0,0 +1,9 @@
+<?php
+    //insertion sort
+    function insertionSort($array){
+        return $array;
+    }
+    $array = [1,2,3,4,5,6];
+    $arraysorted = insertionSort($array);
+    var_dump($arraysorted);
+?>
